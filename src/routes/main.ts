ba6675f0aa71abc219bb7a9757a191e85d7cd5cd
Luan <luan.santos@rspneus.com.br>
2025-04-23 11:06:34 -0300

    Ajustes de inserção de dados

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { prisma } from "../libs/prisma";
+import { createUser } from "../services/user";
 
 export const mainRouter = Router();
 
@@ -7,6 +8,16 @@ mainRouter.get("/ping", (req, res) => {
   res.json({ pong: true });
 });
 
-mainRouter.get("/test", (req, res) => {
-  prisma.user;
+mainRouter.post("/user", async (req, res) => {
+  // Validar dados recebidos
+
+  const user = await createUser({
+    name: "Valdemar",
+    email: "valdemarv778@gmail.com",
+  });
+  if (user) {
+    res.status(201).json({ user });
+  } else {
+    res.status(500).json({ error: "Algo de errado não está certo!" });
+  }
 });
